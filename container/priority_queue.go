53fb6fcc2403e2816deb245044047e73e68dcfbd
levizmli <levizmli@tencent.com>
2023-08-25 14:59:48 +0800

    feat: 移除泛型实现

--- container/priority_queue.go
+++ container/priority_queue.go
@@ -13,66 +13,66 @@ type Comparabler interface {
 }
 
 // PriorityQueue priority queue based container/heap
-type PriorityQueue[T Comparabler] struct {
-	queue []T
-	self  *PriorityQueue[T]
+type PriorityQueue struct {
+	queue []Comparabler
+	self  *PriorityQueue
 }
 
 // InitPq initial a priority queue instance
-func NewPriorityqueue[T Comparabler](size int64) (pq *PriorityQueue[T]) {
-	pq = new(PriorityQueue[T])
-	pq.queue = make([]T, 0, size)
+func NewPriorityqueue(size int64) (pq *PriorityQueue) {
+	pq = new(PriorityQueue)
+	pq.queue = make([]Comparabler, 0, size)
 	pq.self = pq
 	heap.Init(pq)
 	return pq
 }
 
 // Len implement heap interface
-func (pq *PriorityQueue[T]) Len() int {
+func (pq *PriorityQueue) Len() int {
 	return len(pq.queue)
 }
 
 // Less implement heap interface
-func (pq *PriorityQueue[T]) Less(i, j int) bool {
+func (pq *PriorityQueue) Less(i, j int) bool {
 	return pq.queue[i].Less(pq.queue[j])
 }
 
 // Swap implement heap interface
-func (pq *PriorityQueue[T]) Swap(i, j int) {
+func (pq *PriorityQueue) Swap(i, j int) {
 	pq.queue[i], pq.queue[j] = pq.queue[j], pq.queue[i]
 }
 
 // Push implement heap interface
-func (pq *PriorityQueue[T]) Push(x interface{}) {
-	pq.queue = append(pq.queue, x.(T))
+func (pq *PriorityQueue) Push(x interface{}) {
+	pq.queue = append(pq.queue, x.(Comparabler))
 }
 
 // Pop implement heap interface
-func (pq *PriorityQueue[T]) Pop() interface{} {
+func (pq *PriorityQueue) Pop() interface{} {
 	n := len(pq.queue)
 	x := pq.queue[n-1]
-	// pq.queue[n-1] = nil
+	pq.queue[n-1] = nil
 	pq.queue = pq.queue[:n-1]
 	return x
 }
 
 // Pushx push a element into priority queue
-func (pq *PriorityQueue[T]) Pushx(x T) {
+func (pq *PriorityQueue) Pushx(x Comparabler) {
 	heap.Push(pq.self, x)
 }
 
 // Popx pop a element from priority queue, removes and returns the top element
-func (pq *PriorityQueue[T]) Popx() (x T) {
-	x = heap.Pop(pq.self).(T)
+func (pq *PriorityQueue) Popx() (x Comparabler) {
+	x = heap.Pop(pq.self).(Comparabler)
 	return x
 }
 
 // Top returns the top element but don't remove
-func (pq *PriorityQueue[T]) Topx() (x T) {
+func (pq *PriorityQueue) Topx() (x Comparabler) {
 	return pq.queue[len(pq.queue)-1]
 }
 
 // Empty
-func (pq *PriorityQueue[T]) Empty() bool {
+func (pq *PriorityQueue) Empty() bool {
 	return len(pq.queue) == 0
 }
